4bb9fee2dce3a034c8221c3d041125550aa8721c
Ashish Shevale <shevaleashish@gmail.com>
2021-04-27 15:56:10 +0530

    Added the ResultDisplay component
    This component will display the result of calculation

---
+++ src/components/ResultDisplay.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function ResultDisplay(props)
+{
+    
+
+    return (
+        <div style = {div_style}>
+            <h2 style = {h2_style}>0.00</h2>
+        </div>
+    )
+}
+
+export default ResultDisplay
+
