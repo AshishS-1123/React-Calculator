ec4af246c66ff58bf7f934cfd211221ee30edd0c
Ashish Shevale <shevaleashish@gmail.com>
2021-04-27 15:53:45 +0530

    Added the Button Properties JSON file.
    This will determine how each button is rendered in the Calculator

---
+++ src/components/button_props.js
@@ -0,0 +1,134 @@
+/*
+
+Button Props Style
+
+1. id -> unique identifier given for all items
+2. value -> character to be displayed inside the button
+3. type -> type of style to be applied to the button. Number have different styles
+           and special function buttons have different styles
+*/
+
+export const btn_props = [
+    
+    {
+        id: 1, 
+        value: "C",
+        type: "special",
+    },
+
+    {
+        id: 2, 
+        value: "DEL",
+        type: "special",
+    },
+
+    {
+        id: 3, 
+        value: "%",
+        type: "special",
+    },
+    
+    {
+        id: 4, 
+        value: "/",
+        type: "special",
+    },
+
+    {
+        id: 5, 
+        value: "7",
+        type: "num",
+    },
+
+    {
+        id: 6, 
+        value: "8",
+        type: "num",
+    },
+
+    {
+        id: 7, 
+        value: "9",
+        type: "num",
+    },
+    
+    {
+        id: 8, 
+        value: "x",
+        type: "special",
+    },
+
+    {
+        id: 9, 
+        value: "4",
+        type: "num",
+    },
+
+    {
+        id: 10, 
+        value: "5",
+        type: "num",
+    },
+
+    {
+        id: 11, 
+        value: "6",
+        type: "num",
+    },
+    
+    {
+        id: 12, 
+        value: "-",
+        type: "special",
+    },
+
+    {
+        id: 13, 
+        value: "1",
+        type: "num",
+    },
+
+    {
+        id: 14, 
+        value: "2",
+        type: "num",
+    },
+
+    {
+        id: 15, 
+        value: "3",
+        type: "num",
+    },
+    
+    {
+        id: 16, 
+        value: "+",
+        type: "special",
+    },
+
+
+    {
+        id: 17, 
+        value: "",
+        type: "blank",
+    },
+
+    {
+        id: 18, 
+        value: "0",
+        type: "num",
+    },
+
+    {
+        id: 19, 
+        value: ".0",
+        type: "decimal",
+    },
+    
+    {
+        id: 20, 
+        value: "=",
+        type: "special",
+    },
+
+]
