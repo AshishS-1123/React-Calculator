4eeab4d2a16c65912ef30b0af1fd0b9da68d7190
Ashish Shevale <shevaleashish@gmail.com>
2021-04-28 15:56:45 +0530

    Added styles to to Result Display Component

--- src/components/ResultDisplay.js
+++ src/components/ResultDisplay.js
@@ -2,7 +2,33 @@ import React from "react"
 
 function ResultDisplay(props)
 {
-    
+    const div_style = {
+        width: "100%",
+        height: "35%",
+
+        order: "0",
+
+        borderTopRightRadius: "15px", 
+        borderTopLeftRadius: "15px", 
+        border: "3px solid #1976D2", 
+        borderBottom: "0px", 
+
+        backgroundColor: "#1E88E5", 
+
+        textAlign: "bottom",
+        fontSize: "2em",
+
+        paddingRight: "0px", 
+
+        position: "relative", 
+    }
+
+    const h2_style = {
+        margin: "0px", 
+        position: "absolute",
+        bottom: "10px", 
+        right: "15px", 
+    }
 
     return (
         <div style = {div_style}>
