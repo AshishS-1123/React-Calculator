4802aa2682d9c9e23a500ef3ef9790953fb50d91
Ashish Shevale <shevaleashish@gmail.com>
2021-04-28 15:57:53 +0530

    Added the Keypad component
    This component will hold all the keys, numeric and functional in the calculator

---
+++ src/components/Keypad.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Button from "./Button";
+import { btn_props } from "./button_props";
+
+function Keypad(props)
+{
+
+    return (
+        <div className= {"keypad_container"} style = {style}>
+			<Button />
+        </div>
+    )
+}
+
+export default Keypad
