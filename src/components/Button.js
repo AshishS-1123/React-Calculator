2b93af3870ca5c5186ab6f5429e51d237d42b606
Ashish Shevale <shevaleashish@gmail.com>
2021-05-17 16:02:16 +0530

    Added props the Button to load content dynamically

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,13 @@ import React from "react"
 
 function Button(props)
 {
-    
 
     return (
-		<button> 
-		=
+		<button onClick={(e) => props.eventHandler(e)}
+				style = {(props.attributes.type === "special") ? special_style:simple_style} 
+				id={`btn-${props.attributes.value}`}
+		> 
+			{props.attributes.value} 
 		</button>
     )
 }
