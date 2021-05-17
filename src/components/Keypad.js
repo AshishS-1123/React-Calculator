c58c295d6eeeeaae1f122674b9171a11270e63e5
Ashish Shevale <shevaleashish@gmail.com>
2021-05-17 15:59:57 +0530

    Added style to Keypad component

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -5,7 +5,21 @@ import { btn_props } from "./button_props";
 
 function Keypad(props)
 {
-    
+    const style = {
+        order: "1",
+
+        width: "100%",
+        height: "80%",
+
+        display: "flex", 
+        flexWrap: "wrap", 
+		flexDirection: "row", 
+
+        border: "3px solid #1976D2", 
+		padding: "0px", 
+
+        zIndex: "0", 
+    }
 
 	const button_array = btn_props.map( function(property){
 		return <Button key={property.id}
