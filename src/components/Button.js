0553767eed073adfb93961dcd205c1ab101b8b3e
Ashish Shevale <shevaleashish@gmail.com>
2021-05-18 16:02:39 +0530

    Added styles to Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,24 @@ import React from "react"
 
 function Button(props)
 {
+    const simple_style = {
+        width: "25%",
+        height: "20%",
+
+        margin: "0px", 
+
+		zIndex: -10, 
+
+        textAlign: "center", 
+		fontSize: "1.5em", 
+
+		flexBasis: "25%", 
+		order: props.attributes.id, 
+
+    }
+
+	const special_style = {...simple_style, 
+	}
 
     return (
 		<button onClick={(e) => props.eventHandler(e)}
