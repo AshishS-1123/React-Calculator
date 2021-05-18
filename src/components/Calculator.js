ae76da9c90c23ae58890f1d023a9279fe9259acd
Ashish Shevale <shevaleashish@gmail.com>
2021-05-18 16:03:46 +0530

    Updated the Calculator Component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,31 +6,9 @@ import handleEvent from "./EventHandler"
 
 class Calculator extends React.Component {
 
-	constructor(){
-		super()
-
-		
-	}
 
     render()
     {
-
-        const style = {
-            height: "80vh", 
-            width: "50%",
-            minWidth: "400px",
-			maxWidth: "500px", 
-
-            margin: "0 auto",
-            marginTop: "10vh", 
-            
-            display: "flex",
-            flexDirection: "column",
-            position: "relative", 
-
-            borderRadius: "15px", 
-			zIndex: "0", 
-        }
         
         return (
             <div style = {style}>
