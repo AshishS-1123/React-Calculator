73a0cd3f7142d7de309936d55bf44b2f656c225b
Ashish Shevale <shevaleashish@gmail.com>
2021-05-18 16:04:59 +0530

    Added state to Calculator to keep handle click events

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,10 +6,21 @@ import handleEvent from "./EventHandler"
 
 class Calculator extends React.Component {
 
+	constructor(){
+		super()
+
+		this.state = {
+			answer: "0.00",
+			operand_1: {value: 0, is_decimal: false}, 
+			operand_2: {value: 0, is_decimal: false}, 
+			operator: "" 
+		}
+		
+	}
 
     render()
     {
-		
+
 
         const style = {
             height: "80vh", 
