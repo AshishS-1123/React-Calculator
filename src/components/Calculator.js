77d371b96695d6cc450f9ac60c0ac79e2a8cd7e1
Ashish Shevale <shevaleashish@gmail.com>
2021-05-18 16:04:18 +0530

    Added styles to Calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,6 +9,24 @@ class Calculator extends React.Component {
 
     render()
     {
+		
+
+        const style = {
+            height: "80vh", 
+            width: "50%",
+            minWidth: "400px",
+			maxWidth: "500px", 
+
+            margin: "0 auto",
+            marginTop: "10vh", 
+            
+            display: "flex",
+            flexDirection: "column",
+            position: "relative", 
+
+            borderRadius: "15px", 
+			zIndex: "0", 
+        }
         
         return (
             <div style = {style}>
